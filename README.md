85ffe9d1f31afcf76ba2b3c94b6cdd5867410c20
Hyphen133 <hyphen133@gmail.com>
2020-05-14 18:28:49 +0200

    Spectral entropy added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Gender-by-Voice-Recognition
+
+### Installation 
+
+-Spectral entropy
+git clone https://github.com/raphaelvallat/entropy.git entropy/
+cd entropy/
+pip install -r requirements.txt
+python setup.py develop
--- sounds_processing.py
+++ sounds_processing.py
@@ -1,6 +1,8 @@
 import librosa as librosa
 import numpy as np
 import matplotlib.pyplot as plt
+from entropy import entropy
+from scipy import stats
 
 data, sampling_rate = librosa.load('friday-rocks.wav')
 
@@ -10,15 +12,31 @@ frequency_components = np.abs(frequency_components)
 frequency_components = frequency_components[:int(sampling_rate/2)]
 frequency_values = np.linspace(0, len(frequency_components), len(frequency_components))
 
-frequency_components = frequency_components[:280]
+#Converting to kHz
+frequency_components = [fc/1000 for fc in frequency_components[:280]]
 frequency_values = frequency_values[:280]
 
 plt.plot(frequency_values, frequency_components)
 plt.show()
 
 mean_freq = sum([ frequency_values[i] * frequency_components[i] for i in range(len(frequency_values))])/sum(frequency_components)
-mean_freq = mean_freq/1000
+std_freq = np.std(frequency_values)
+median_freq = np.median(frequency_values)
+q25 = np.quantile(frequency_values, 0.25)
+q75 = np.quantile(frequency_values, 0.75)
+iqr = stats.iqr(frequency_values)
+skew = stats.skew(data)
+kurt = stats.kurtosis(data)
+spectral_entropy = entropy.spectral_entropy(data, sampling_rate, normalize=True)
 
 print("Mean frequency: " + str(mean_freq))
+print("Standard deviation: " + str(std_freq))
+print("Median: " + str(median_freq))
+print("Q25: " + str(q25))
+print("Q75: " + str(q75))
+print("IQR: " + str(iqr))
+print("Skew: " + str(skew))
+print("Kurt: " + str(kurt))
+print("Spectral Entropy: " + str(spectral_entropy))
 
 print()
